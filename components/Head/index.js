e0630b578a0a41311da272272eaaee33f3e015f0
nave91 <rekojtoor@gmail.com>
2020-08-17 23:38:35 -0400

    Working html fixes

--- components/Head/index.js
+++ components/Head/index.js
@@ -5,6 +5,7 @@ export default () => (
         <Head>
             <title>Naveen</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name="description" content="Naveen Kumar Lekkalapudi. Software Engineer at Fishtown Analytics. Ex Facebook, Bellhops."/>
         </Head>
     </div>
 )
